75dcbe7d76afb3fea79fce42423eebf7f740c273
Kardam <kardam@Kardams-MacBook-Pro.local>
2026-02-04 00:20:00 +0530

    Fix model path using absolute path for cloud deployment

--- src/predict.py
+++ src/predict.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import joblib
+import os
 from src.preprocess import preprocess_data
 
-def predict(input_data: dict):
-    model = joblib.load("model.pkl")
+# Absolute-safe path (works locally + Docker + Render)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
 
+model = joblib.load(MODEL_PATH)
+
+
+def predict(input_data: dict):
     df = pd.DataFrame([input_data])
+
     X_scaled, _ = preprocess_data(df, training=False)
 
     prediction = model.predict(X_scaled)[0]
